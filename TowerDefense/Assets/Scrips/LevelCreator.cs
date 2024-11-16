214a1fe8f8a8e55d9e9776f4ddd72ebdeee1f817
R2D2RDZ <62351594+R2D2RDZ@users.noreply.github.com>
2024-11-15 23:06:36 -0600

    Map creation and NavPoint array

---
+++ TowerDefense/Assets/Scrips/LevelCreator.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class LevelCreator : MonoBehaviour
+{
+    //int[,] gridlock = new int[18,10];
+
+    string filepath = "Assets\\Level0.txt";
+
+    [SerializeField] GameObject[] Square;
+
+    public NavPoint[] NavPoints;
+    void Start()
+    {
+
+        if (File.Exists(filepath))
+        {
+            try
+            {
+                string[] lineas = File.ReadAllLines(filepath);
+                NavPoints = new NavPoint[int.Parse(lineas[0])+1];
+                // Procesar cada línea y asignar valores a la matriz
+                for (int i = 0; i < 18; i++) // Filas
+                {
+                    //Debug.Log(lineas[i+1]);
+                    string[] valores = lineas[i+1].Split(new char[] { ' ' });
+
+                    for (int j = 0; j < 10; j++) // Columnas
+                    {
+                        Debug.LogWarning(valores[j]);
+                        if (int.Parse(valores[j]) == 0)
+                        {
+                            Instantiate(Square[0], new Vector3(i, j, 0), Quaternion.identity);
+                        }
+                        else {
+                            Instantiate(Square[1], new Vector3(i, j, 0), Quaternion.identity);
+                            NavPoints[int.Parse(valores[j])] = new NavPoint(new Vector2(i,j));
+                            //int.Parse(valores[j])
+                            //Debug.LogError(NavPoints[int.Parse(valores[j])].position);
+                        }
+                    }
+                }
+
+                // Mostrar la matriz cargada
+                Debug.Log("Matriz cargada desde el archivo:");
+            }
+            catch (Exception ex)
+            {
+                Debug.Log("Error al leer el archivo o procesar los datos: " + ex.Message);
+            }
+        }
+        else
+        {
+            Debug.Log("El archivo especificado no existe.");
+            Debug.Log(Directory.GetCurrentDirectory());
+        }
+    }
+
+}
+
+public class NavPoint {
+    public Vector2 position;
+
+    public NavPoint(Vector2 position)
+    {
+        this.position = position;
+    }
+}
+
