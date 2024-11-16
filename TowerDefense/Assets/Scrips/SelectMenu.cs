f68dd4a2c8cfaf2755537e0b9efc85fd481e3a9c
R2D2RDZ <62351594+R2D2RDZ@users.noreply.github.com>
2024-11-16 12:58:12 -0600

    Menu created

--- TowerDefense/Assets/Scrips/SelectMenu.cs
+++ TowerDefense/Assets/Scrips/SelectMenu.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 
 public class SelectMenu : MonoBehaviour
 {
-    public GameObject dropdownMenuPrefab; // Assign your dropdown menu prefab here
+    [SerializeField] GameObject dropdownMenuPrefab; // Assign your dropdown menu prefab here
+    [SerializeField] GameObject optionPrefab;
+
     private GameObject currentDropdownMenu;
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0)) // Left-click
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
+            Debug.Log("Casting ray");
 
-            if (Physics.Raycast(ray, out hit)) // Detect collider under the mouse
+            if (hit.collider != null) // Detect collider under the mouse
             {
+                Debug.Log("Hit: " + hit.collider.gameObject.name);
                 // Destroy existing menu if one exists
                 if (currentDropdownMenu != null)
                 {
@@ -40,9 +44,24 @@ public class SelectMenu : MonoBehaviour
                     );
 
                     menuRect.anchoredPosition = canvasPoint;
+                    CreateOptions(hit.collider.tag, currentDropdownMenu);
                 }
                 
             }
+        }
+    }
+
+    void CreateOptions(string tag, GameObject menu)
+    {
+        switch (tag) {
+            case "Free": 
+                GameObject Turret = Instantiate(optionPrefab, menu.transform);
+                Turret.GetComponent<RectTransform>().position = menu.GetComponent<RectTransform>().position + Vector3.up * 30;
+                GameObject Pylon = Instantiate(optionPrefab, menu.transform);
+                Pylon.GetComponent<RectTransform>().position = menu.GetComponent<RectTransform>().position + Vector3.left * 30;
+                GameObject Light = Instantiate(optionPrefab, menu.transform);
+                Light.GetComponent<RectTransform>().position = menu.GetComponent<RectTransform>().position + Vector3.right * 30;
+                break;
         }
     }
 }
