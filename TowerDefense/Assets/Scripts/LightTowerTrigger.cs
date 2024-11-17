031cb8fab93c29a1259b09a086196170bde2396f
davedt97 <davedt97@hotmail.com>
2024-11-16 22:52:47 -0600

    Señor si señor ahora si esta el hacker

--- TowerDefense/Assets/Scripts/LightTowerTrigger.cs
+++ TowerDefense/Assets/Scripts/LightTowerTrigger.cs
@@ -2,26 +2,50 @@ using UnityEngine;
 
 public class LightTower : MonoBehaviour
 {
+    public LightClass luz;
+
+    private void Start()
+    {
+        luz = GetComponent<LightClass>();
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // Aca estoy verificando si el objeto que entra al collider tiene un componente "EnemyAttributes"
-        var enemy = other.GetComponent<EnemyAttributes>();
-        Debug.Log("caca");
-        if (enemy != null)
+        if (luz.isEnabled)
         {
-            // Hace visible al enemigo para las unidades ofensivas, porque si no no lo ven y no pueden atacar
-            enemy.isVisible = true;
+            // Aca estoy verificando si el objeto que entra al collider tiene un componente "EnemyAttributes"
+            var enemy = other.GetComponent<EnemyAttributes>();
+            if (enemy != null)
+            {
+                // Hace visible al enemigo para las unidades ofensivas, porque si no no lo ven y no pueden atacar
+                enemy.isVisible = true;
+            }
+        }
+        else
+        {
+            var enemy = other.GetComponent<EnemyAttributes>();
+            enemy.isVisible = false;
         }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        // Aca estoy verificando si el objeto que entra al collider tiene un componente "EnemyAttributes"
-        var enemy = other.GetComponent<EnemyAttributes>();
-        if (enemy != null)
+        if (luz.isEnabled)
         {
-            // Hace visible al enemigo para las unidades ofensivas, porque si no no lo ven y no pueden atacar
-            enemy.isVisible = false;
+            // Aca estoy verificando si el objeto que entra al collider tiene un componente "EnemyAttributes"
+            var enemy = other.GetComponent<EnemyAttributes>();
+            if (enemy != null)
+            {
+                // Hace visible al enemigo para las unidades ofensivas, porque si no no lo ven y no pueden atacar
+                enemy.isVisible = false;
+            }
+        }
+        else
+        {
+            var enemy = other.GetComponent<HackerAttributes>();
+            if(enemy != null)
+            {
+                luz.isEnabled = true;
+            }
         }
     }
 }
