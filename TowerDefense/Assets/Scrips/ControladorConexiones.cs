2d86844c2cf1c7e5efe8659df99ba27d5720cca5
davedt97 <davedt97@hotmail.com>
2024-11-17 14:31:53 -0600

    Ya jala

--- TowerDefense/Assets/Scrips/ControladorConexiones.cs
+++ TowerDefense/Assets/Scrips/ControladorConexiones.cs
@@ -106,9 +106,11 @@ using static UnityEditor.PlayerSettings;
 
 public class ControladorConexiones : MonoBehaviour
 {
+
+    
     private NewFaros primeraBobina;
     public LineRenderer cablePrefab;
-    public LayerMask LayerMask;
+    public LayerMask layerMask;
     private List<LineRenderer> cables = new List<LineRenderer>(); // Lista para almacenar los cables creados
 
     void Update()
@@ -116,15 +118,17 @@ public class ControladorConexiones : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             
-            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, -Mathf.Infinity,LayerMask);
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, -Mathf.Infinity,layerMask);
 
             if (hit.collider != null)
             {
+                Debug.Log("bobinas ray: " + hit.collider.gameObject.name);
                 // Comprobar si el clic fue en un cable
                 LineRenderer cableSeleccionado = hit.collider.GetComponent<LineRenderer>();
                 if (cableSeleccionado != null)
                 {
                     DestruirCable(cableSeleccionado);
+                    primeraBobina = null;
                     return;
                 }
 
@@ -141,7 +145,7 @@ public class ControladorConexiones : MonoBehaviour
                     else
                     {
                         // Conectar las Bobinas y crear el cable
-                        primeraBobina.Conectar(bobinaSeleccionada);
+                        //primeraBobina.Conectar(bobinaSeleccionada);
                         CrearCable(primeraBobina, bobinaSeleccionada);
                         primeraBobina = null;
                         Debug.Log(bobinaSeleccionada.name);
@@ -153,26 +157,38 @@ public class ControladorConexiones : MonoBehaviour
 
     void CrearCable(MonoBehaviour origen, MonoBehaviour destino)
     {
-        LineRenderer cable = Instantiate(cablePrefab);
-        cable.positionCount = 2;
-        float radio = Vector3.Distance(origen.transform.position, destino.transform.position) /2;
-        cable.SetPosition(0, Vector3.left * radio);
-        cable.SetPosition(1, Vector3.right * radio);
-        cable.transform.position = (origen.transform.position + destino.transform.position) / 2;
+        bool asa = origen.GetComponent<NewFaros>().Conectar(destino.GetComponent<NewFaros>());
+        bool sas = destino.GetComponent<NewFaros>().Conectar(origen.GetComponent<NewFaros>());
+        Debug.Log("" + asa + " " + sas + "" + (asa && sas));
+        if (asa && sas)
+        {
+            LineRenderer cable = Instantiate(cablePrefab);
+            cable.gameObject.GetComponent<LineData>().origin = origen.gameObject;
+            cable.gameObject.GetComponent<LineData>().target = destino.gameObject;
+            cable.positionCount = 2;
+            float radio = Vector3.Distance(origen.transform.position, destino.transform.position) / 2;
+            cable.SetPosition(0, Vector3.left * radio);
+            cable.SetPosition(1, Vector3.right * radio);
+            cable.transform.position = (origen.transform.position + destino.transform.position) / 2;
 
-        // Agregar un Collider2D al cable para detectar clics y colisiones
-        BoxCollider2D collider = cable.gameObject.AddComponent<BoxCollider2D>();
-        collider.isTrigger = true;
-        collider.size = new Vector2(Vector3.Distance(origen.transform.position, destino.transform.position), 0.1f);
-        //collider.transform.position = (origen.transform.position + destino.transform.position) / 2;
-        collider.transform.rotation = Quaternion.FromToRotation(Vector3.right, destino.transform.position - origen.transform.position);
+            // Agregar un Collider2D al cable para detectar clics y colisiones
+            BoxCollider2D collider = cable.gameObject.AddComponent<BoxCollider2D>();
+            collider.isTrigger = true;
+            collider.size = new Vector2(Vector3.Distance(origen.transform.position, destino.transform.position), 0.1f);
+            //collider.transform.position = (origen.transform.position + destino.transform.position) / 2;
+            collider.transform.rotation = Quaternion.FromToRotation(Vector3.right, destino.transform.position - origen.transform.position);
 
-        cables.Add(cable); // Añadir el cable a la lista
+            cables.Add(cable); // Añadir el cable a la lista
+        }
+
     }
 
     void DestruirCable(LineRenderer cable)
     {
-        if ()
+        NewFaros origin = cable.gameObject.GetComponent<LineData>().origin.GetComponent<NewFaros>();
+        NewFaros destino = cable.gameObject.GetComponent<LineData>().target.GetComponent<NewFaros>();
+        origin.Desconectar(destino);
+        destino.Desconectar(origin);
         cables.Remove(cable);
         Destroy(cable.gameObject);
     }
