3a81654406352bceb046841513b17dfd52b5fc11
¨¨Gustavo <gustavoaduran@hotmail.com>
2024-11-16 23:56:24 -0600

    musica

--- TowerDefense/Assets/Scrips/Guns/GunClass.cs
+++ TowerDefense/Assets/Scrips/Guns/GunClass.cs
@@ -19,7 +19,7 @@ public class GunClass : MonoBehaviour {
 	//[SerializeField]
 	public float cost;
 	public float energyConsumed = 1;
-	public static bool isOn = true;
+	public static bool isOn = false;
 	//[SerializeField]
 	public GameObject projectile;
 	public GameObject flash;
