7c636f97e18722e065e845bc7363ef4eb6a9e665
ZeroLullably <karen_yosoy3890@hotmail.com>
2024-11-17 16:04:40 -0600

    Final

---
+++ TowerDefense/Assets/Scrips/SceneChanger.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // Necesario para usar SceneManager
+
+public class SceneChanger : MonoBehaviour
+{
+    // Método para cambiar a la escena del nivel 1
+    public void CambiarANivel1()
+    {
+        SceneManager.LoadScene("Test Map");
+    }
+
+    // Método para salir del juego
+    public void Salir()
+    {
+        Application.Quit(); // Cierra el juego cuando se ejecuta
+        Debug.Log("El juego se ha cerrado."); // Esto solo se verá en el editor de Unity
+    }
+
+    // Método para volver al nivel 1 desde la escena de "Game Over"
+    public void VolverAlInicio()
+    {
+        Time.timeScale = 1; // Restablece el tiempo en caso de que el juego esté pausado
+        SceneManager.LoadScene("Test Map"); // Carga la escena del nivel 1
+    }
+
+    public void VolverAlMenuPrincipal()
+    {
+        Time.timeScale = 1; // Restablece el tiempo en caso de que el juego esté pausado
+        SceneManager.LoadScene("PantalladeInicio"); // Asegúrate de que el nombre de la escena es correcto
+    }
+}
+
+
